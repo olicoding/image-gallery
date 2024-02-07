8673fa9e3e926275f503f13c9eca63a3f02ca3a9
olicoding <oliveira.sein@gmail.com>
2024-02-07 06:04:30 +0100

    Bug-fix: Resolving hydration issues.

--- src/components/GalleryPerDevice.jsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { isMobile } from "react-device-detect";
-import GalleryDesktop from "./GalleryDesktop";
-import GalleryMobile from "./GalleryMobile";
-
-export default function GalleryPerDevice() {
-  return <>{isMobile ? <GalleryMobile /> : <GalleryDesktop />}</>;
-}
